a98d740615998ba911106c352c534c34f451285e
Jorge E Gomez <jeg1986@gmail.com>
2023-07-19 08:44:45 -0300

    Ejercicio 1: Imprimir nombre por consola.

---
+++ clases_javascript/index.js
@@ -0,0 +1,10 @@
+// Practica 
+
+// 1 - Imprimir nombre por consola 
+// 2 - Sumar dos números.
+// 3 - concatenar nombre y edad
+// 4 - comparar dos números si es mayor o menor.
+
+let nombre = 'JEG';
+console.log(nombre);
+
