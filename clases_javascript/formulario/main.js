ad4d7e0670fb74a98f8b78d70fbe37c08165c6dc
Jorge E Gomez <jeg1986@gmail.com>
2023-08-15 16:51:38 -0300

    Función buscar.

--- clases_javascript/formulario/main.js
+++ clases_javascript/formulario/main.js
@@ -29,21 +29,25 @@ const addFormPerson = () => {
   console.log(dataPerson);
   document.getElementById("person").reset();
   dni.focus();
-  showDataPerson();
+  showDataPerson(dataPerson);
 }
 
-const showDataPerson = () => {
+const showDataPerson = (arrayDataPerson) => {
   let listTable = '';
-  dataPerson.forEach((person) => {
-    let status = returnStatusPerson(parseInt(person.status));
-    listTable += `<tr>
-      <td>${person.dni}</td>
-      <td>${person.firstName} ${person.lastName}</td>
-      <td>${person.birthday}</td>
-      <td>${person.address}</td>
-      <td>${status}</td>
-    </tr>`;
+  if(arrayDataPerson.length > 0) {
+    arrayDataPerson.forEach((person) => {
+      let status = returnStatusPerson(parseInt(person.status));
+      listTable += `<tr>
+        <td>${person.dni}</td>
+        <td>${person.firstName} ${person.lastName}</td>
+        <td>${person.birthday}</td>
+        <td>${person.address}</td>
+        <td>${status}</td>
+      </tr>`;
   });
+}else {
+  listTable += `<tr><td colspan="5" class="text-center">No se encuentran registros</td></tr>`;
+}
   document.getElementById("listPerson").innerHTML = listTable;
 }
 
@@ -54,4 +58,24 @@ const returnStatusPerson = (status) => {
     case 2: return 'Concluido'; break;
     default: return 'Activo'; break;
   }
+}
+
+const searchFilter = () => {
+  let search = document.getElementById("search").value;
+  search = search.trim().toLowerCase();
+  document.getElementById("search").value = search;
+
+  if(search.length > 0) {
+    let dataFilter = dataFilterPerson(search);
+    showDataPerson(dataFilter);
+  } else {
+    showDataPerson(dataPerson);
+  }
+}
+
+const dataFilterPerson = (words) => {
+  let resultFilter = dataPerson.filter((person) => {
+    return person.firstName.toLowerCase().includes(words) || person.lastName.toLowerCase().includes(words);
+  });
+  return resultFilter;
 }
